63c9dacf6e0acced87dee5a41ed92dcf499ae03b
sharonjs27 <jimenezsegurasharonstephany@gmail.com>
2023-04-14 23:30:16 -0600

    implementando singleton

--- funciones.py
+++ funciones.py
@@ -1,20 +1,73 @@
 from tkinter import *
+
 root = Tk()
 root.geometry("400x400")
 root.title("***JUEGA Y APRENDE***")
 
 ########### FUNCIONES ###################
+
+class Singleton:
+    _instance = None
+    def __init__(self):
+        print("Llamado al metodo Init")
+        self.nombre = "Soy Unico"
+
+    def __new__(cls, *args, **kwargs):
+
+        if not isinstance(cls._instance, cls):
+            print("Llamado al metodo nueva instancia")
+            cls._instance = object.__new__(cls)
+
+        return cls._instance
+
+    def getNombre(self):
+        print("Llamdo a obtener atributo nombre")
+        print(self.nombre)
+
+    def setValor(self, valor):
+        self.nombre = valor
+
+class unicoArchivo():
+    _instance = None
+    file = None
+
+    def __init__(self, NombreArchivo):
+        self.matrix = [[1,2],[3,4],[5,6]]
+        if self.file is None:
+         self.file = open(NombreArchivo, 'w')
+
+    def __new__(cls, *args, **kwargs):
+
+        if not isinstance(cls._instance, cls):
+            print("Creando el unico archivo")
+            cls._instance = object.__new__(cls)
+
+        return cls._instance
+
+    def agregar_a_matriz(self, linea):
+        self.file.write(linea + '\n')
+
+
+
 def exit_program():
     root.destroy()
 def clic_boton1():
-    texto = Label(root, text="no lo vuelvas a precionar").grid(row=1, column=3)
+    texto = Label(root, text="Bienvenido a Español").grid(row=1, column=3)
+    boton6 = Button(root, text="APRENDAMOS", bg="ORANGE", width=10, height=3, command=clic_boton1).grid(row=1,column=5)
+    boton7 = Button(root, text="JUGUEMOS", bg="ORANGE", width=10, height=3, command=clic_boton1).grid(row=1, column=7)
+
 def clic_boton2():
-    texto = Label(root, text="hola").grid(row=2, column=3)
+    texto = Label(root, text="Bienvenido a Ciencias").grid(row=2, column=3)
+    file = unicoArchivo("ciencias.txt")
+    file = open('ciencias.txt', 'w')
+    file.write('material de ciencias ')
+    file.close()
+
 def clic_boton3():
-    texto = Label(root, text="adios").grid(row=3, column=3)
+    texto = Label(root, text="Bienvenido a Mate").grid(row=3, column=3)
 
 def clic_boton4():
-        texto = Label(root, text="buenas").grid(row=4, column=3)
+        texto = Label(root, text="Bienvenido a Est. Sociales").grid(row=4, column=3)
 
 
 ############ CREACION DE LOS BOTONES PARA EL MENU #########
@@ -29,5 +82,7 @@ boton5 =Button(root,text="SALIR", bg="PURPLE",width=10,height=4, command=exit_pr
 
 
 # Configurar la ventana principal
+
+
 root.mainloop()
 
